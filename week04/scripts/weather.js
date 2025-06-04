fbba383c1e57c6fcd871ea2e64f0e90ecf8ef2be
KingKacrae99 <kacrae9@gmail.com>
2025-06-04 20:57:04 +0100

    Home page added

---
+++ week04/scripts/weather.js
@@ -0,0 +1,22 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=51.15&lon=10.64&units=metric&appid=10db7d4bffa3c6ef8f5a503de083d7bd';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
