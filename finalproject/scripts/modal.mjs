7c3de1c933cbb16c753b050b44f31bcfe81e049e
KingKacrae99 <kacrae9@gmail.com>
2025-06-23 17:03:16 +0100

    MediCheck updated successfully

--- finalproject/scripts/modal.mjs
+++ finalproject/scripts/modal.mjs
@@ -1,56 +1,64 @@
 import { getDrugList } from "./drug.mjs";
 export default function showDrugInfo(drug){
     const verifyDetails = document.querySelector('#verify-details');
-    if(drug){
-        function Verified(drug){
-            if(drug.verified){
-                return "Verified ✅";
-            } else{
-                return "Drug's not Authentic";
-            }
-        }
-
-        verifyDetails.innerHTML = `
-            <button id="closeModal">X</button>
-            <h3>${drug.name}</h3>
-            <p><strong>Batch Number </strong> ${drug.batch}</p>
-            <p><strong>Manufacturer</strong> ${drug.manufacturer}</p>
-            <p><strong>Expiry</strong> ${drug.expiry}</p>
-            <p><strong>NafDac No</strong> ${drug.nafdac_no}</p>
-            <p><strong>Verified</strong> ${Verified(drug)}</p>
-            <div class="save-option">
-            Would you like add this drug to your drug list? 
-            <button id="saveitem">yes</button> <button id="dontsave">No</button>
-            </div>
-            `
-    } else{
-        verifyDetails.innerHTML =`
-            <button id="closeModal">X</button>
-            <h3>Drug not found!</h3>`;
-    }
-    
+    const loader = document.querySelector(".loader");
+    const content = document.querySelector(".drug-content");
     verifyDetails.showModal()
-    closeModal.addEventListener('click', () =>{
-        verifyDetails.close()
-    });
-    saveitem.addEventListener('click', () => {
-        if (localStorage.getItem(drug.name)) {
-            verifyDetails.innerHTML =`
-            <button id="closeModal">X</button>
-            <h3>This drug is already saved in your drug list</h3>`;
-            
-        } else {
-            localStorage.setItem(drug.name, JSON.stringify(drug))
-            console.log(JSON.stringify(drug))
-            verifyDetails.innerHTML = `
-                <button id="closeModal2">X</button>
-                <h3>Drug has been added to list</h3>`
+    loader.classList.remove("loader-hidden")
+    content.style.display ='none';
+
+    setTimeout(()=>{
+        if(drug){
+            loader.classList.remove("loader")
+            loader.classList.add("loader-hidden");
+            content.style.display = "block";
+
+            function Verified(drug){
+                if(drug.verified){
+                    return "Verified ✅";
+                } else{
+                    return "Drug's not Authentic";
+                }
+            }
+            content.innerHTML = `
+                <h3>${drug.name}</h3>
+                <p><strong>Batch Number </strong> ${drug.batch}</p>
+                <p><strong>Manufacturer</strong> ${drug.manufacturer}</p>
+                <p><strong>Expiry</strong> ${drug.expiry}</p>
+                <p><strong>NafDac No</strong> ${drug.nafdac_no}</p>
+                <p><strong>Verified</strong> ${Verified(drug)}</p>
+                <div class="save-option">
+                Would you like add this drug to your drug list? 
+                <button id="saveitem">yes</button> <button id="dontsave">No</button>
+                </div>
+                `
+        } else{
+            content.innerHTML =`
+                <button id="closeModal">X</button>
+                <h3>Drug not found!</h3>`;
         }
+        
+    saveitem.addEventListener('click', () => {
+    if (localStorage.getItem(drug.name)) {
+        content.innerHTML =`
+        <h3>This drug is already saved in your drug list</h3>`;
+        
+    } else {
+        localStorage.setItem(drug.name, JSON.stringify(drug))
+        console.log(JSON.stringify(drug))
+        content.innerHTML = `
+            <h3>Drug has been added to list</h3>`
+    }
     });
     dontsave.addEventListener('click', () =>{
         verifyDetails.close()
     });
-    closeModal2.addEventListener('click', () =>{
+    
+    },750)
+
+    closeModal.addEventListener('click', () =>{
         verifyDetails.close()
-    })
+    });
+
+    
 }
